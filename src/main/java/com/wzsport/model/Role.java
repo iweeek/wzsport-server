97d15c6bf5fa236a64897209df00f13ad28a5e10
张佳耿 <x1nyhh@163.com>
2017-05-15 10:59:31 +0800

    see #348_3

--- src/main/java/com/wzsport/model/Role.java
+++ src/main/java/com/wzsport/model/Role.java
@@ -1,5 +1,9 @@
 package com.wzsport.model;
 
+import graphql.Scalars;
+import graphql.schema.GraphQLFieldDefinition;
+import graphql.schema.GraphQLObjectType;
+
 /**
 * @ClassName: Role
 * @Description: 角色
@@ -11,6 +15,26 @@ public class Role {
 	private int id;
 	private String name;
 	
+	/**
+	* @author x1ny
+	* @date 2017年5月15日
+	* @Description: 创建相应的GraphQL类型
+	* @return
+	* @throws
+	*/
+	public static GraphQLObjectType createGraphQLObjectType() {
+		return GraphQLObjectType.newObject()
+				.name("Role")
+				.field(GraphQLFieldDefinition.newFieldDefinition()
+						.name("id")
+						.type(Scalars.GraphQLInt)
+						.build())
+				.field(GraphQLFieldDefinition.newFieldDefinition()
+						.name("name")
+						.type(Scalars.GraphQLString)
+						.build())
+				.build();
+	}
 	public int getId() {
 		return id;
 	}

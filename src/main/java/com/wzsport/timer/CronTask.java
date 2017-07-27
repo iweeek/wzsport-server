91b0a717313cda4bddd59d0d69e958a4be8a3b26
chaiyu <chaiyu2002@gmail.com>
2017-07-27 15:08:58 +0800

    修改了表结构，对RunningSport增加了sampleNum

--- src/main/java/com/wzsport/timer/CronTask.java
+++ src/main/java/com/wzsport/timer/CronTask.java
@@ -1,7 +1,14 @@
 package com.wzsport.timer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import com.wzsport.mapper.RunningActivityDataMapper;
+import com.wzsport.mapper.RunningActivityMapper;
+import com.wzsport.service.RunningActivityService;
 
 
 @Component
@@ -13,10 +20,10 @@ public class CronTask {
 	@Autowired
 	RunningActivityService runningActivityService;
 	
-	private static final Logger logger = LoggerFactory.getLogger(FixLocationOutdoorSportPointsController.class);
-	
-    @Scheduled(cron = "0 0 1 * * ?")  
-    public void job() {  
-
-    } 
+//	private static final Logger logger = LoggerFactory.getLogger(CronTask.class);
+//	
+//    @Scheduled(cron = "0 0 1 * * ?")  
+//    public void job() {  
+//
+//    } 
 }

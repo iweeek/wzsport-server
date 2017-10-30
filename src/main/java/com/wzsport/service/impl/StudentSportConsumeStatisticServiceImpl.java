0f4a35d4a6c7bd9c69b6bcd1e0660a5c27f0c33f
chaiyu <chaiyu2002@gmail.com>
2017-10-30 12:11:10 +0800

    see #1111_1

--- src/main/java/com/wzsport/service/impl/StudentSportConsumeStatisticServiceImpl.java
+++ src/main/java/com/wzsport/service/impl/StudentSportConsumeStatisticServiceImpl.java
@@ -9,49 +9,71 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.wzsport.mapper.StudentSportConsumeDailyStatisticMapper;
 import com.wzsport.mapper.StudentSportConsumeStatisticMapper;
+import com.wzsport.model.StudentSportConsumeDailyStatistic;
 import com.wzsport.model.StudentSportConsumeStatistic;
 import com.wzsport.service.StudentSportConsumeStatisticService;
-import com.wzsport.service.StatisticTaskService;
 
 @Service
 public class StudentSportConsumeStatisticServiceImpl implements StudentSportConsumeStatisticService {
-	@Autowired
-	private StudentSportConsumeStatisticMapper studentSportConsumeStatisticMapper;
+    @Autowired
+    private StudentSportConsumeStatisticMapper studentSportConsumeStatisticMapper;
+    
+    @Autowired
+    private StudentSportConsumeDailyStatisticMapper studentSportConsumeDailyStatisticMapper;
 
-	private static final Logger logger = LogManager.getLogger(StudentSportConsumeStatisticServiceImpl.class);
-	@Override
-	@Transactional
-	public boolean create() {
-		Calendar c = Calendar.getInstance();
-		Calendar calFirst = Calendar.getInstance();
-		Calendar calLast = Calendar.getInstance();
+    private static final Logger logger = LogManager.getLogger(StudentSportConsumeStatisticServiceImpl.class);
+    @Override
+    @Transactional
+    public boolean create() {
+        Calendar c = Calendar.getInstance();
+        Calendar calFirst = Calendar.getInstance();
+        Calendar calLast = Calendar.getInstance();
 
-		int today = 	c.get(Calendar.DAY_OF_MONTH);
-		// 如果是本月第一天，取上个月第一天和最后一天,否则，取本月
-		if (today == 1) {
-			calFirst.add(Calendar.MONTH, -1); // 上一个月
-			calFirst.set(Calendar.DAY_OF_MONTH, 1);
+        int today = c.get(Calendar.DAY_OF_MONTH);
+        // 如果是本月第一天，取上个月第一天和最后一天,否则，取本月
+        if (today == 1) {
+            calFirst.add(Calendar.MONTH, -1); // 上一个月
+            calFirst.set(Calendar.DAY_OF_MONTH, 1);
 
-			calLast.set(Calendar.DAY_OF_MONTH,0);
-		} else {
-			calFirst.add(Calendar.MONTH, 0);
-			calFirst.set(Calendar.DAY_OF_MONTH,1);
+            calLast.set(Calendar.DAY_OF_MONTH, 0);
+        } else {
+            calFirst.add(Calendar.MONTH, 0);
+            calFirst.set(Calendar.DAY_OF_MONTH, 1);
 
-			calLast.set(Calendar.DAY_OF_MONTH, calLast.getActualMaximum(Calendar.DAY_OF_MONTH));
-		}
-		List<StudentSportConsumeStatistic> list =
-				studentSportConsumeStatisticMapper.getStudentSportConsumeStatisticList(calFirst.getTime(),calLast.getTime());
+            calLast.set(Calendar.DAY_OF_MONTH, calLast.getActualMaximum(Calendar.DAY_OF_MONTH));
+        }
+        
+        List<StudentSportConsumeStatistic> list =
+            studentSportConsumeStatisticMapper.getStudentSportConsumeStatisticList(calFirst.getTime(), calLast.getTime());
 
-		for (StudentSportConsumeStatistic studentSportConsumeStatistic : list) {
-			try {
-				studentSportConsumeStatisticMapper.insert(studentSportConsumeStatistic);
-			} catch (Exception e) {
-				logger.error(e);
-			}
-		}
-
-		return true;
-	}
+        for (StudentSportConsumeStatistic studentSportConsumeStatistic : list) {
+            try {
+                studentSportConsumeStatisticMapper.insert(studentSportConsumeStatistic);
+            } catch (Exception e) {
+                logger.error(e);
+            }
+        }
+        
+        List<StudentSportConsumeDailyStatistic> runningDailylist = studentSportConsumeDailyStatisticMapper.getRunningSportDailyList(calFirst.getTime(), calLast.getTime());
+        for (StudentSportConsumeDailyStatistic runningSport : runningDailylist) {
+            try {
+                studentSportConsumeDailyStatisticMapper.insert(runningSport);
+            } catch (Exception e) {
+                logger.error(e);
+            }
+        }
+        
+        List<StudentSportConsumeDailyStatistic> areaDailylist = studentSportConsumeDailyStatisticMapper.getAreaSportDailyList(calFirst.getTime(), calLast.getTime());
+        for (StudentSportConsumeDailyStatistic areaSport : areaDailylist) {
+            try {
+                studentSportConsumeDailyStatisticMapper.insert(areaSport);
+            } catch (Exception e) {
+                logger.error(e);
+            }
+        }
+        return true;
+    }
 
 }

b4876e80b1fe7216cae7c955041d71b05be17ed6
SmartNi <nijun717@gmail.com>
2017-11-06 09:41:30 +0800

    统计

--- src/main/java/com/wzsport/service/impl/StudentSportConsumeStatisticServiceImpl.java
+++ src/main/java/com/wzsport/service/impl/StudentSportConsumeStatisticServiceImpl.java
@@ -9,8 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.wzsport.mapper.ClassMapper;
+import com.wzsport.mapper.CollegeMapper;
+import com.wzsport.mapper.MajorMapper;
 import com.wzsport.mapper.StudentSportConsumeDailyStatisticMapper;
 import com.wzsport.mapper.StudentSportConsumeStatisticMapper;
+import com.wzsport.model.Class;
+import com.wzsport.model.College;
+import com.wzsport.model.Major;
 import com.wzsport.model.StudentSportConsumeDailyStatistic;
 import com.wzsport.model.StudentSportConsumeStatistic;
 import com.wzsport.service.StudentSportConsumeStatisticService;
@@ -19,11 +25,19 @@ import com.wzsport.service.StudentSportConsumeStatisticService;
 public class StudentSportConsumeStatisticServiceImpl implements StudentSportConsumeStatisticService {
     @Autowired
     private StudentSportConsumeStatisticMapper studentSportConsumeStatisticMapper;
-    
+
     @Autowired
     private StudentSportConsumeDailyStatisticMapper studentSportConsumeDailyStatisticMapper;
 
+    @Autowired
+    private ClassMapper classMapper;
+    @Autowired
+    private MajorMapper majorMapper;
+    @Autowired
+    private CollegeMapper collegeMapper;
+
     private static final Logger logger = LogManager.getLogger(StudentSportConsumeStatisticServiceImpl.class);
+
     @Override
     @Transactional
     public boolean create() {
@@ -44,19 +58,35 @@ public class StudentSportConsumeStatisticServiceImpl implements StudentSportCons
 
             calLast.set(Calendar.DAY_OF_MONTH, calLast.getActualMaximum(Calendar.DAY_OF_MONTH));
         }
-        
-        List<StudentSportConsumeStatistic> list =
-            studentSportConsumeStatisticMapper.getStudentSportConsumeStatisticList(calFirst.getTime(), calLast.getTime());
+
+        calFirst.add(Calendar.MONTH, 0);
+        calFirst.set(Calendar.DAY_OF_MONTH, 5);
+        calLast.set(Calendar.DAY_OF_MONTH, calLast.getActualMaximum(Calendar.DAY_OF_MONTH));
+
+        List<StudentSportConsumeStatistic> list = studentSportConsumeStatisticMapper
+                .getStudentSportConsumeStatisticList(calFirst.getTime(), calLast.getTime());
 
         for (StudentSportConsumeStatistic studentSportConsumeStatistic : list) {
             try {
+                
+                Class classInfo = classMapper.selectByPrimaryKey(studentSportConsumeStatistic.getClassId());
+                studentSportConsumeStatistic.setGrade(classInfo.getGrade());
+                studentSportConsumeStatistic.setMajorId(classInfo.getMajorId());
+                studentSportConsumeStatistic.setMajorName(classInfo.getName());
+
+                Major majorInfo = majorMapper.selectByPrimaryKey(classInfo.getMajorId());
+                College collegeInfo = collegeMapper.selectByPrimaryKey(majorInfo.getCollegeId());
+                studentSportConsumeStatistic.setCollegeId(collegeInfo.getId());
+                studentSportConsumeStatistic.setCollegeName(collegeInfo.getName());
                 studentSportConsumeStatisticMapper.insert(studentSportConsumeStatistic);
+//                studentSportConsumeStatisticMapper./xmheart_pc_serverupdateByPrimaryKey(studentSportConsumeStatistic);
             } catch (Exception e) {
                 logger.error(e);
             }
         }
-        
-        List<StudentSportConsumeDailyStatistic> runningDailylist = studentSportConsumeDailyStatisticMapper.getRunningSportDailyList(calFirst.getTime(), calLast.getTime());
+
+        List<StudentSportConsumeDailyStatistic> runningDailylist = studentSportConsumeDailyStatisticMapper
+                .getRunningSportDailyList(calFirst.getTime(), calLast.getTime());
         for (StudentSportConsumeDailyStatistic runningSport : runningDailylist) {
             try {
                 studentSportConsumeDailyStatisticMapper.insert(runningSport);
@@ -64,8 +94,9 @@ public class StudentSportConsumeStatisticServiceImpl implements StudentSportCons
                 logger.error(e);
             }
         }
-        
-        List<StudentSportConsumeDailyStatistic> areaDailylist = studentSportConsumeDailyStatisticMapper.getAreaSportDailyList(calFirst.getTime(), calLast.getTime());
+
+        List<StudentSportConsumeDailyStatistic> areaDailylist = studentSportConsumeDailyStatisticMapper
+                .getAreaSportDailyList(calFirst.getTime(), calLast.getTime());
         for (StudentSportConsumeDailyStatistic areaSport : areaDailylist) {
             try {
                 studentSportConsumeDailyStatisticMapper.insert(areaSport);
@@ -75,5 +106,4 @@ public class StudentSportConsumeStatisticServiceImpl implements StudentSportCons
         }
         return true;
     }
-
 }
